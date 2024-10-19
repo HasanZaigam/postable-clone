3338753cb54f291f983fd3202381430e1677d6bf
Hasan Zaigam <syedhasanzaigam50890@gmail.com>
2024-10-19 12:43:35 +0530

    apos fix again

--- components/FAQSection.tsx
+++ components/FAQSection.tsx
@@ -265,7 +265,7 @@ const FAQSection = () => {
       <div className="relative flex w-full flex-col gap-4 md:flex-row md:gap-24">
         <div className="max-w-sm">
           <div className="hidden w-max flex-col md:flex">
-            <span className="text-lg font-medium text-neutral-500">Couldn't find your answer?</span>
+            <span className="text-lg font-medium text-neutral-500">Couldn&apos;t find your answer?</span>
             <a target="_blank" className="flex items-center gap-1 text-md font-medium text-purple-400" href="https://forms.gle/DAowWbDE9fNKh8Ji8">
               Ask a Question 
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-arrow-right size-3">
